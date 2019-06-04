1b620b12ae4b53c3857fdd8af5c2294547ed6a75
albertofragoso <hola@albertofragoso.com>
2019-06-03 22:28:43 -0500

    Configurando el servidor, el archivo .env y Middlewares.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
-const express = require('express')
-const app = express()
+require('dotenv').config()
+
+const express      = require('express')
+const app          = express()
+const cookieParser = require('cookie-parser')
+const mongoose     = require('mongoose')
+
+mongoose
+  .connect(process.env.DB, { userNewUrlParser: true }, err => {
+    if(err) return err
+    console.log('Conented to MongoDB,')
+  })
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cookieParser())
+
 const port = process.env.PORT || 3002
 
 app.listen(port, () => console.log(`The server is running on ${port} port.`))
