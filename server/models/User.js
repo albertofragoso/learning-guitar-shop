4b59460e689510010a954e77b69125316de6d34b
albertofragoso <hola@albertofragoso.com>
2019-06-03 23:16:21 -0500

    Creando el modelo User.

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: 1
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  name: {
+    type: String,
+    required: true,
+    maxlength: 100
+  },
+  lastname: {
+    type: String,
+    required: true,
+    maxlength: 100
+  },
+  cart: {
+    type: Array,
+    default: []
+  },
+  history: {
+    type: Array,
+    default: []
+  },
+  role: {
+    type: Number,
+    default: 0
+  },
+  token: {
+    type: String
+  }
+}, 
+{
+  timestamps: true,
+  versionKey: false
+})
+
+module.exports = mongoose.model('User', userSchema)
--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ const express      = require('express')
 const app          = express()
 const cookieParser = require('cookie-parser')
 const mongoose     = require('mongoose')
+const User         = require('./models/User')
 
 mongoose
-  .connect(process.env.DB, { userNewUrlParser: true }, err => {
+  .connect('mongodb://localhost:27017/tiendaguitarras', { userNewUrlParser: true }, err => {
     if(err) return err
     console.log('Conented to MongoDB,')
   })
@@ -18,3 +19,11 @@ app.use(cookieParser())
 const port = process.env.PORT || 3002
 
 app.listen(port, () => console.log(`The server is running on ${port} port.`))
+
+app.post('/api/user/register', (req, res, next) => {
+  const user = new User(req.body)
+  user.save((err, doc) => {
+    if(err) return res.json({ success: false, err })
+    res.status(200).json({ success: true, userdata: doc })
+  })
+})
